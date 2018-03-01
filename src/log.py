227972a43e45073255a7241ed4f10f0b22177cf2
blackenedgem <Chrisroberts97@btinternet.com>
2018-03-01 19:45:24 +0000

    Fix spaces/tabs

--- src/log.py
+++ src/log.py
@@ -21,10 +21,10 @@ class log():
     @staticmethod
     def log(logMode: logModes, text: str):
         if log.shouldLog(logMode):
-			try:
-				print(text)
-			except UnicodeEncodeError as e:
-				print(e)
+            try:
+                print(text)
+            except UnicodeEncodeError as e:
+                print(e)
 
     # Check if we should log (for more advanced print statements)
     @staticmethod
